0acae926e5d32c831ac34991c9c0d46b770532cc
muhammad-imran-developer <167766582+muhammad-imran-developer@users.noreply.github.com>
2026-08-24 15:00:38 +0500

    test git configuration

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+using System.Text;
+
+namespace MyApp;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        int num = 12;
+
+        ref int numref = ref num;
+
+        Console.WriteLine(num);
+
+        numref = 20;
+        Console.WriteLine(numref);
+
+        Console.WriteLine('Hello world');
+
+
+    }
+
+
+}
