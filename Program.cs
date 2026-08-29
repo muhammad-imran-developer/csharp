6c29077c19a81c8ff867c5f9b91ce848254c39df
muhammad-imran-developer <167766582+muhammad-imran-developer@users.noreply.github.com>
2026-08-29 16:31:42 +0500

    Exception Handling

--- Program.cs
+++ Program.cs
@@ -8,18 +8,91 @@ namespace MyApp;
 
 class Program
 {
-    // Normal method call mein tum kehte ho "ye method abhi chalao"; delegate mein tum kehte ho "is method ka reference rakho, main ise baad mein ya kisi aur jagah se chalaunga."
+
+
+
+    /* ===================================================================
+
+    // Program ko is tarah likhna ke error aaye to application crash hone ke bajaye us error ko properly handle kare.
+    =======================================================================
+    Exception	                        Example
+    NullReferenceException	            null object ko access karna
+    DivideByZeroException	            Zero se divide
+    FormatException	                    Invalid string conversion
+    IndexOutOfRangeException	        Array ke invalid index ko access
+    ArgumentException	                Invalid argument
+    ArgumentNullException	            Required argument null
+    InvalidOperationException	        Invalid operation/state
+    FileNotFoundException	            File nahi mili
+    =========================================================================
+    // try ke andar woh code likhte hain jahan exception occur hone ka chance ho.
+    //(Finally) Cleanup code jo normally execute hona chahiye, chahe exception aaye ya na aaye.
+ 
+============================
+BASIC STRUCTURE OF EXCEPTION
+============================
+
+    try
+{
+    // risky code
+}
+catch (Exception ex)
+{
+    // handle error
+}
+finally
+{
+    // cleanup
+}
+
+      =======================================================================  */
     static void Main(string[] args)
     {
-        Person.Alert alert = Person.Desktop;
-
-        alert += Person.Laptop;
-        alert += Person.Mobile;
 
 
-        alert.Invoke("OK");
+        try
+        {
+            int a = 10;
+            int b = 0;
+
+            int result = a / b;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
 
 
+        try
+        {
+            int number = int.Parse("abc");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Wrong format.");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Number is too large.");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Unknown error.");
+        }
+
+
+        try
+        {
+            int number = int.Parse("abc");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Please enter a valid number.");
+        }
+        finally
+        {
+            Console.WriteLine("Finished.");
+        }
 
 
     }
