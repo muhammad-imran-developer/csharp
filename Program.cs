824e4dbbbc49b0210d71451dbe1f5ca6a03b9c27
muhammad-imran-developer <167766582+muhammad-imran-developer@users.noreply.github.com>
2026-08-29 16:42:45 +0500

    Exception hanling Practice

--- Program.cs
+++ Program.cs
@@ -8,93 +8,67 @@ namespace MyApp;
 
 class Program
 {
-
-
-
-    /* ===================================================================
-
-    // Program ko is tarah likhna ke error aaye to application crash hone ke bajaye us error ko properly handle kare.
-    =======================================================================
-    Exception	                        Example
-    NullReferenceException	            null object ko access karna
-    DivideByZeroException	            Zero se divide
-    FormatException	                    Invalid string conversion
-    IndexOutOfRangeException	        Array ke invalid index ko access
-    ArgumentException	                Invalid argument
-    ArgumentNullException	            Required argument null
-    InvalidOperationException	        Invalid operation/state
-    FileNotFoundException	            File nahi mili
-    =========================================================================
-    // try ke andar woh code likhte hain jahan exception occur hone ka chance ho.
-    //(Finally) Cleanup code jo normally execute hona chahiye, chahe exception aaye ya na aaye.
- 
-============================
-BASIC STRUCTURE OF EXCEPTION
-============================
-
-    try
-{
-    // risky code
-}
-catch (Exception ex)
-{
-    // handle error
-}
-finally
-{
-    // cleanup
-}
-
-      =======================================================================  */
     static void Main(string[] args)
     {
+        // try
+        // {
+        //     int a = 10;
+        //     int b = 0;
+
+        //     int result = a / b;
+        // }
+
+        // catch (Exception ex)
+        // {
+        //     Console.WriteLine(ex.Message);
+        // }
 
 
+        // static void SetHealth(int health)
+        // {
+        //     if (health < 0)
+        //     {
+        //         throw new Exception("Health cannot be negative.");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine($"Health: {health}");
+        //     }
+
+
+        // }
+
+        // try
+        // {
+        //     SetHealth(110);
+        // }
+        // catch (Exception ex)
+        // {
+        //     Console.WriteLine(ex.Message);
+        // }
+
+
+        static void CheckAge(int Age)
+        {
+            if (Age < 0)
+            {
+                throw new Exception("Age cannot be negative.");
+            }
+            else
+            {
+                Console.WriteLine($"AGE: {Age}");
+            }
+
+
+        }
         try
         {
-            int a = 10;
-            int b = 0;
-
-            int result = a / b;
+            CheckAge(2);
         }
         catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
         }
-
-
-        try
-        {
-            int number = int.Parse("abc");
-        }
-        catch (FormatException)
-        {
-            Console.WriteLine("Wrong format.");
-        }
-        catch (OverflowException)
-        {
-            Console.WriteLine("Number is too large.");
-        }
-        catch (Exception)
-        {
-            Console.WriteLine("Unknown error.");
-        }
-
-
-        try
-        {
-            int number = int.Parse("abc");
-        }
-        catch (FormatException)
-        {
-            Console.WriteLine("Please enter a valid number.");
-        }
-        finally
-        {
-            Console.WriteLine("Finished.");
-        }
-
-
     }
 
 
