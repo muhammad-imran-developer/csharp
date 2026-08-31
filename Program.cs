e06ce3099f1ad08addabdb25c97cf246004290c2
muhammad-imran-developer <167766582+muhammad-imran-developer@users.noreply.github.com>
2026-08-31 11:20:39 +0500

    Generic Collections Practice

--- Program.cs
+++ Program.cs
@@ -6,31 +6,91 @@ using System.Text;
 
 namespace MyApp;
 
-// Generics/Templated Classes and Methods
+// Generics Collections
 class Program
 {
     static void Main(string[] args)
     {
+        //Generic Collections Lists and Dictionary and Hashset and Queue and Stack and Linked List
+        // List<int> ints = [1, 2, 3, 4, 5, 6];
+        // ints.Add(12);
 
-        // Templated Method
-        T DoubleIt<T>(T number) where T : INumber<T>
+        // ints.ForEach((int n) =>
+        // {
+        //     Console.WriteLine(n);
+        // });
+
+        // ints.ForEach(Console.Write);
+
+        // ints.Find((int n) => n == 2);
+        // ints.Contains(12);
+        // ints.AddRange([7, 8, 9, 10, 11]);
+
+
+        // foreach (var each in ints)
+        // {
+        //     Console.WriteLine(each);
+        // }
+        // Console.WriteLine(ints.Capacity);
+
+        //     Dictionary<string, List<int>> grades = new()
+        //     {
+        //         {"me", [1,1,1]},
+        //         {"you", [2,2,2]},
+
+
+        // };
+
+        // grades["him"] = [3, 3, 3];
+
+        // if (grades.TryGetValue("me", out List<int>? myGrades))
+        // {
+        //     foreach (var grade in myGrades)
+        //     {
+        //         Console.WriteLine(grade);
+        //     }
+
+        // }
+        // if (grades.TryGetValue("him", out List<int>? myGrades))
+        // {
+        //     foreach (var grade in myGrades)
+        //     {
+        //         Console.WriteLine(grade);
+        //     }
+
+        // }
+
+        // Hashset except unique values but it not throught error it override the same key with previous
+
+        // HashSet<string> names = ["me", "me", "you"];
+
+        // foreach (var name in names)
+        // {
+        //     Console.WriteLine(name);
+        // }
+
+        // QUEUE
+
+        // Queue<string> queue = [];
+        // queue.Enqueue("");
+        // queue.Dequeue();
+
+
+        // Stack<int> stack = [];
+        // stack.Push(13);
+        // stack.Pop();
+
+
+        LinkedList<int> ints = [];
+        ints.AddFirst(2);
+        ints.AddLast(3);
+
+        foreach (var i in ints)
         {
-            return (dynamic)number * 2;
+            Console.WriteLine(i);
+
         }
 
-        var twice = DoubleIt(12); // yaha per jo value dein gay T os value ki data type mein convert ho jay ga ya 12 diya h to ab her T ki jaga int likha jay ga
-        Console.WriteLine(twice);
 
     }
-
-    // Templated Classes
-
-    class Vector<T, T2> //int
-    {
-        T data; // int data
-    }
-
-    Vector<int, float> vector = new(); // it replace T with int and T2 with float
-    Vector<double, string> vector2 = new(); // it rplace T with double and T2 with string
-
 }
